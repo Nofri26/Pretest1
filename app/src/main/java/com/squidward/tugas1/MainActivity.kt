75cfc75766366266678909103533efe299e9d331
Nofrizal <1zhal9991@gmail.com>
2020-04-04 19:17:24 +0700

    Initial commit

---
+++ app/src/main/java/com/squidward/tugas1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.squidward.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
